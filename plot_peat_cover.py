2fa75082053972fd7d5ed8bc9c8b4b553eb16cba
Keren Mezuman <km2961@columbia.edu>
2025-03-05 17:05:38 -0500

    updating file path and variable name

--- plot_peat_cover.py
+++ plot_peat_cover.py
@@ -1,1 +1,3 @@
-S144x90_SoilGrids250m2.0_ETOPO1_CongoPeat2_ext.nc
+path='/discover/nobackup/projects/giss/prod_input_files'
+var='peat_cover'
+filename='S144x90_SoilGrids250m2.0_ETOPO1_CongoPeat2_ext.nc'
